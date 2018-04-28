0294ed558068627560d0aa6a3c423a6dffbe5d01
oskar <oskar.asbrink@gmail.com>
2018-04-28 14:10:25 +0200

    corner magic

---
+++ client/js/worker/ui/elements/GuiCornerElement.js
@@ -0,0 +1,85 @@
+"use strict";
+
+define([
+        'ConfigObject',
+        'ui/elements/EffectList'
+    ],
+    function(
+        ConfigObject,
+        EffectList
+    ) {
+
+        var GuiCornerElement = function() {
+
+            this.obj3d = new THREE.Object3D();
+
+            this.cornerPassiveEffect = new EffectList();
+            this.cornerActiveEffect = new EffectList();
+        };
+
+        GuiCornerElement.prototype.initCornerElement = function(onReadyCB) {
+
+            var configLoaded = function() {
+                onReadyCB(this);
+            }.bind(this);
+
+            this.configObject = new ConfigObject('GUI_ELEMENTS', 'GUI_CORNER_ELEMENT', 'config');
+            this.configObject.addCallback(configLoaded);
+
+        };
+
+        GuiCornerElement.prototype.configRead = function(dataKey) {
+            return this.configObject.getConfigByDataKey(dataKey)
+        };
+
+        GuiCornerElement.prototype.enableCornerEffects = function(effectList, dataKey) {
+            if (effectList.effectCount() === 0) {
+                effectList.enableEffectList(this.configRead(dataKey), this.obj3d.position, this.obj3d.quaternion);
+            }
+        };
+
+        GuiCornerElement.prototype.disableCornerEffects = function(effectList) {
+            effectList.disableEffectList();
+        };
+
+        GuiCornerElement.prototype.setCornerActive = function(bool, dataKey) {
+
+            if (bool) {
+                this.enableCornerEffects(this.cornerActiveEffect, dataKey)
+            } else {
+                this.disableCornerEffects(this.cornerActiveEffect)
+            }
+
+        };
+
+        GuiCornerElement.prototype.setCornerPassive = function(bool, dataKey) {
+
+            if (bool) {
+                this.enableCornerEffects(this.cornerPassiveEffect, dataKey)
+            } else {
+                this.disableCornerEffects(this.cornerPassiveEffect)
+            }
+        };
+
+
+        GuiCornerElement.prototype.setCornerXY = function(x, y) {
+            this.obj3d.position.set(x, y, -1);
+        };
+
+        GuiCornerElement.prototype.setCornerQuaternion = function(quat) {
+            this.obj3d.quaternion.copy(quat);
+        };
+
+        GuiCornerElement.prototype.getCornerQuaternion = function() {
+            return this.obj3d.quaternion;
+        };
+
+        GuiCornerElement.prototype.updateSurfaceElement = function(surfaceData) {
+
+            this.testPointerHover(surfaceData)
+
+        };
+
+        return GuiCornerElement;
+
+    });
--- client/js/worker/ui/elements/GuiSurfaceElement.js
+++ client/js/worker/ui/elements/GuiSurfaceElement.js
@@ -2,10 +2,12 @@
 
 define([
         'ConfigObject',
+        'ui/elements/GuiCornerElement',
         'ui/elements/EffectList'
     ],
     function(
         ConfigObject,
+        GuiCornerElement,
         EffectList
     ) {
 
@@ -18,19 +20,22 @@ define([
 
             this.obj3d = new THREE.Object3D();
 
-
-
-            this.topLeft = new EffectList();
-            this.topRight = new EffectList();
-            this.bottomLeft = new EffectList();
-            this.bottomRight = new EffectList();
+            this.topLeft = new GuiCornerElement();
+            this.topRight = new GuiCornerElement();
+            this.bottomLeft = new GuiCornerElement();
+            this.bottomRight = new GuiCornerElement();
 
         };
 
         GuiSurfaceElement.prototype.initSurfaceElement = function(onReadyCB) {
 
-            var configLoaded = function() {
+            var cornersReady = function() {
                 onReadyCB(this);
+            }.bind(this);
+
+
+            var configLoaded = function() {
+                this.initSurfaceCorners(cornersReady);
             }.bind(this);
 
             this.configObject = new ConfigObject('GUI_ELEMENTS', 'GUI_SURFACE_ELEMENT', 'config');
@@ -38,55 +43,89 @@ define([
 
         };
 
+        GuiSurfaceElement.prototype.initSurfaceCorners = function(cornersReady) {
+            
+
+            var br = function(crnr) {
+                crnr.getCornerQuaternion().set(0, 0, -1, 0);
+                cornersReady();
+            }.bind(this);
+
+            var bl = function(crnr) {
+                crnr.getCornerQuaternion().set(1, 0, 0, 0);
+                this.bottomRight.initCornerElement(br)
+            }.bind(this);
+
+            var tr = function(crnr) {
+                crnr.getCornerQuaternion().set(0, 1, 0, 0);
+                this.bottomLeft.initCornerElement(bl)
+            }.bind(this);
+
+            var tl = function(crnr) {
+                crnr.getCornerQuaternion().set(0, 0, 0, 1);
+                this.topRight.initCornerElement(tr)
+            }.bind(this);
+
+            this.topLeft.initCornerElement(tl)
+
+        };
 
         GuiSurfaceElement.prototype.configRead = function(dataKey) {
             return this.configObject.getConfigByDataKey(dataKey)
         };
 
-        GuiSurfaceElement.prototype.highlightCorner = function(effectList, posVec, dataKey) {
-            if (effectList.effectCount() === 0) {
-                effectList.enableEffectList(this.configRead(dataKey), posVec);
-            }
-            effectList.setEffectListPosition(posVec)
+        GuiSurfaceElement.prototype.applySurfaceData = function(surfaceData) {
+            this.topLeft.setCornerXY(surfaceData.left, surfaceData.top);
+            this.topRight.setCornerXY(surfaceData.right, surfaceData.top);
+            this.bottomLeft.setCornerXY(surfaceData.left, surfaceData.bottom);
+            this.bottomRight.setCornerXY(surfaceData.right, surfaceData.bottom);
         };
 
+        GuiSurfaceElement.prototype.applySurfacePassive = function(bool, dataKey) {
+            this.topLeft.setCornerPassive(bool, dataKey);
+            this.topRight.setCornerPassive(bool, dataKey);
+            this.bottomLeft.setCornerPassive(bool, dataKey);
+            this.bottomRight.setCornerPassive(bool, dataKey);
+        };
+
+        GuiSurfaceElement.prototype.applySurfaceActive = function(bool, dataKey) {
+            this.topLeft.setCornerActive(bool, dataKey);
+            this.topRight.setCornerActive(bool, dataKey);
+            this.bottomLeft.setCornerActive(bool, dataKey);
+            this.bottomRight.setCornerActive(bool, dataKey);
+        };
 
         GuiSurfaceElement.prototype.testPointerHover = function(surfaceData) {
 
             cursor_x = WorldAPI.sampleInputBuffer(ENUMS.InputState.MOUSE_X);
             cursor_y = WorldAPI.sampleInputBuffer(ENUMS.InputState.MOUSE_Y);
 
-            if (cursor_x < surfaceData.left) {
-                tempVec1.set(surfaceData.left, surfaceData.top, -1);
-                this.highlightCorner(this.topLeft, tempVec1, surfaceData.corner_fx)
-            } else {
-                this.topLeft.disableEffectList();
+            var surfaceAvtive = false;
+            var surfacePassive = false;
+
+            if (cursor_x > surfaceData.left && cursor_x < surfaceData.right && cursor_y > surfaceData.bottom && cursor_y < surfaceData.top) {
+                surfacePassive = true;
             }
 
-            if (cursor_x > surfaceData.right) {
-                tempVec1.set(surfaceData.right, surfaceData.top, -1);
-                this.highlightCorner(this.topRight, tempVec1, surfaceData.corner_fx)
-            } else {
-                this.topRight.disableEffectList();
+            if (WorldAPI.sampleInputBuffer(ENUMS.InputState.ACTION_0)) {
+
+                cursor_x = WorldAPI.sampleInputBuffer(ENUMS.InputState.START_DRAG_X);
+                cursor_y = WorldAPI.sampleInputBuffer(ENUMS.InputState.START_DRAG_Y);
+
+                if (cursor_x > surfaceData.left && cursor_x < surfaceData.right && cursor_y > surfaceData.bottom && cursor_y < surfaceData.top) {
+                    surfaceAvtive = true;
+                }
+
             }
 
-            if (cursor_y < surfaceData.top) {
-                tempVec1.set(surfaceData.left, surfaceData.bottom, -1);
-                this.highlightCorner(this.bottomLeft, tempVec1, surfaceData.corner_fx)
-            } else {
-                this.bottomLeft.disableEffectList();
-            }
-
-            if (cursor_y > surfaceData.bottom) {
-                tempVec1.set(surfaceData.right, surfaceData.bottom, -1);
-                this.highlightCorner(this.bottomRight, tempVec1, surfaceData.corner_fx)
-            } else {
-                this.bottomRight.disableEffectList();
-            }
+            this.applySurfacePassive(surfacePassive, surfaceData.corner_passive_fx);
+            this.applySurfaceActive(surfaceAvtive, surfaceData.corner_active_fx);
 
         };
 
         GuiSurfaceElement.prototype.updateSurfaceElement = function(surfaceData) {
+
+            this.applySurfaceData(surfaceData);
 
             this.testPointerHover(surfaceData)
 
