da5ed36a54af898ac1156f797cbe98737165dfb9
oskar <oskar.asbrink@gmail.com>
2018-05-22 00:44:38 +0200

    fx tweaks

--- client/js/worker/controlable/ModuleEffectFunctions.js
+++ client/js/worker/controlable/ModuleEffectFunctions.js
@@ -12,6 +12,7 @@ define([
         var speed;
 
         var tempVec1 = new THREE.Vector3();
+        var tempVec2 = new THREE.Vector3();
         var fxPos = new THREE.Vector3();
         var fxVel = new THREE.Vector3();
         var fxQuat = new THREE.Quaternion();
@@ -31,6 +32,14 @@ define([
             evt.fire(evt.list().WATER_EFFECT, fxArgs);
         };
 
+
+        var randomPosInShape = function(renderable, shape, store) {
+            store.set(shape.size.x * (Math.random()-0.5),  shape.size.y * (Math.random()-0.5), shape.size.z * (Math.random()-0.5));
+            store.applyQuaternion(renderable.quat);
+            store.applyQuaternion(shape.rotation);
+        };
+
+
         ModuleEffectFunctions.propellerEffect = function(renderable, moduleState, trgt) {
 
             speed = renderable.getDynamicSpeed();
@@ -41,7 +50,14 @@ define([
 
             target = renderable.getSpatialShapeById(trgt);
             target.calculateWorldPosition(renderable.pos, renderable.quat, fxArgs.pos);
-            fxArgs.pos.y -= 6.5;
+
+            randomPosInShape(renderable, target, tempVec2);
+
+            fxArgs.pos.add(tempVec2);
+
+            if (fxArgs.pos.y > 2) return;
+
+            fxArgs.pos.y -= 2.5;
             fxArgs.vel.set(2*(Math.random()-0.5), 1, -1*(Math.random()+0.5));
 
             fxArgs.vel.applyQuaternion(renderable.quat);
@@ -59,6 +75,11 @@ define([
 
             target = renderable.getSpatialShapeById(trgt);
             target.calculateWorldPosition(renderable.pos, renderable.quat, fxArgs.pos);
+
+            randomPosInShape(renderable, target, tempVec2);
+            fxArgs.pos.add(tempVec2);
+            if (fxArgs.pos.y > 2) return;
+
             fxArgs.quat.copy(tempObj.quaternion);
             fxArgs.pos.y = 1.5;
             fxArgs.vel.set(0, 0, 0);
@@ -75,23 +96,30 @@ define([
 
             speed = renderable.getDynamicSpeed();
 
+            if (Math.random() * 5 > speed-1) {
+                return;
+            }
 
             target = renderable.getSpatialShapeById(trgt);
             renderable.getDynamicSpatialVelocity(tempVec1);
             target.calculateWorldPosition(renderable.pos, renderable.quat, fxArgs.pos);
 
+
             fxArgs.vel.set(1*(Math.random()-0.5), 1, -1*(Math.random()+0.5));
-
-
-            if (Math.random() * 5 > speed-1) {
-                return;
-            }
 
             fxArgs.vel.applyQuaternion(renderable.quat);
 
-            fxArgs.pos.y = -2;
-            tempVec1.multiplyScalar(Math.random()*1);
+            tempVec1.multiplyScalar(Math.random()*0.5);
             fxArgs.pos.add(tempVec1);
+
+            randomPosInShape(renderable, target, tempVec2);
+
+            fxArgs.pos.add(tempVec2);
+
+            if (fxArgs.pos.y > 2) return;
+
+            fxArgs.pos.y = -2;
+
             spawnTargetEffect(renderable, target, 'prop_effect');
 
             if (Math.random() * 520 > speed -1) {
@@ -105,8 +133,6 @@ define([
             spawnTargetEffect(renderable, target, 'bow_splash');
 
 
-
-
         };
 
         ModuleEffectFunctions.wakeEffect = function(renderable, moduleState, trgt) {
@@ -116,12 +142,16 @@ define([
                 return;
             }
 
+
             target = renderable.getSpatialShapeById(trgt);
             renderable.getDynamicSpatialVelocity(tempVec1);
             target.calculateWorldPosition(renderable.pos, renderable.quat, fxArgs.pos);
 
             fxArgs.vel.set(1*(Math.random()-0.5), 1, -1*(Math.random()+0.5));
 
+            randomPosInShape(renderable, target, tempVec2);
+            if (fxArgs.pos.y > 2) return;
+            fxArgs.pos.add(tempVec2);
             fxArgs.pos.y = 1.5;
             spawnTargetEffect(renderable, target, 'bow_splash');
             fxArgs.vel.applyQuaternion(renderable.quat);
